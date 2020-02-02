40d0dceba277114f2beb356667db661032249d76
Truman <56100185+trumanbe01@users.noreply.github.com>
2020-02-01 21:11:10 -0500

    Commented out getCoords.js

--- getCoords.js
+++ getCoords.js
@@ -1,20 +1,29 @@
+//Pushes the newest set of coordinates into a list of coordinates, called coords.
 function addCoordinates(coords, address, radius) {
     singleCoordinate = retrieveCoordinates(address);
+    //Only push the set of coordinates if they are an array.
     if (typeof(singleCoordinate) == "object") {
         singleCoordinate.push(radius);
         coords.push(singleCoordinate);
     }
 }
 
+//Retrieves and verifies the coordinates may be a match to what the user wants.
 function retrieveCoordinates(address) {
+    //Invokes the method that retrieves the JSON and parses it.
     var obj = JSON.parse(retrieveJSON(address));
+    //Two lists that encompass the possible types of locations entered (i.e. city, state).
     var regions = ["locality", "sublocality", "postal_code", "country", "administrative_level_1", "administrative_level_2"];
     var cities = ["locality", "administrative_area_3"];
+    //If the JSON retrieves has 0 results, exit the function.
     if(obj.results.length == 0) {
         return;
     }
+    //Iterate through each of the types that the first result can be classified as, and verify that it
+    //may either be a city or country.
     for (var x in obj.results[0].types) {
         if (regions.includes(obj.results[0].types[x]) || cities.includes(obj.results[0].types[x])) {
+            //Organizes the location's corresponding coordinates into a list and returns it.
             longitude = obj.results[0].geometry.location.lng;
             latitude = obj.results[0].geometry.location.lat;
             var coordinates = [longitude, latitude];
@@ -23,15 +32,19 @@ function retrieveCoordinates(address) {
     }
 }
 
+//Encodes a web address into the correct formatting to retrieve its corresponding JSON.
 function encodeAddress(city, state) {
     address = "https://maps.googleapis.com/maps/api/geocode/json?components=";
     address = address + "locality:" + city + "|";
     address = address + "administrative_area_level_1:" + state;
+    //Adds the API key onto the end.
     address = address + "&key=AIzaSyCEp7beuCu9-5XxRZ0u7gcVSkIRui4n8oc";
     return address;
 }
 
-//https://stackoverflow.com/questions/2499567/how-to-make-a-json-call-to-a-url/2499647#2499647
+//Requests and receives a JSON.
+//Source:
+//  https://stackoverflow.com/questions/2499567/how-to-make-a-json-call-to-a-url/2499647#2499647
 function retrieveJSON(address) {
     var Httpreq = new XMLHttpRequest(); // a new request
     Httpreq.open("GET",address,false);
@@ -39,26 +52,12 @@ function retrieveJSON(address) {
     return Httpreq.responseText;          
 }
 
-//userAddress = encodeAddress(city, state);
-//addCoordinates(coords, userAddress, 2);
 
+//==========TESTING==========
 city = "Mississauga";
 state = "Ontario";
-userAddress = encodeAddress(city, state) + "&key=AIzaSyCEp7beuCu9-5XxRZ0u7gcVSkIRui4n8oc";
-addCoordinates(coords, userAddress, 2);
+addCoordinates(coords, encodeAddress(city, state), radius);
 
 city = "Detroit";
 state = "Michigan";
-userAddress = encodeAddress(city, state) + "&key=AIzaSyCEp7beuCu9-5XxRZ0u7gcVSkIRui4n8oc";
-addCoordinates(coords, userAddress, 2);
-
-//printString = ""
-
-// for (var i in coords) {
-//     printString = printString + "["
-
-//     for (var j in coords[i]) {
-//         printString = printString + coords[i][j] + "|"
-//     }
-
-//     printString = printString + "]"
+addCoordinates(coords, encodeAddress(city, state), 2);
